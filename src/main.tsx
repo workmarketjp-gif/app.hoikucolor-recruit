b72d482b18d6f7d18c7e4eb6ca56ad33e0816676
workmarketjp-gif <workmarket.jp@gmail.com>
2026-09-06 16:04:40 +1200

    feat: add React entry point

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { AppRoot } from './AppRoot';
+import './styles.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <AppRoot />
+  </StrictMode>,
+);
