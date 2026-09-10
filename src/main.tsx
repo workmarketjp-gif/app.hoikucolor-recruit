aeef8a339aa3bcd60d74143f4015772a946a65d5
workmarketjp-gif <workmarket.jp@gmail.com>
2026-09-10 23:05:11 +1200

    style(color): load verified workplace presentation

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { AppRoot } from './AppRoot';
 import './styles.css';
+import './verified-workplace.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
