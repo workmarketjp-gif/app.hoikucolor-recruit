7f4ad166c2c9190049df17e986262e318aab2d0d
workmarketjp-gif <workmarket.jp@gmail.com>
2026-09-12 17:04:18 +1200

    feat: route scouts to dedicated jobseeker page

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { AppRoot } from './AppRoot';
+import { ScoutRouteRoot } from './ScoutRouteRoot';
 import './styles.css';
 import './verified-workplace.css';
 
+const root = window.location.pathname.startsWith('/scouts') ? <ScoutRouteRoot /> : <AppRoot />;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppRoot />
+    {root}
   </StrictMode>,
 );
