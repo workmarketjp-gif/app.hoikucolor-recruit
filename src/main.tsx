9a0e1833f34a190cab5724538a40c7c647a485b6
workmarketjp-gif <workmarket.jp@gmail.com>
2026-09-12 17:59:56 +1200

    feat: mount scout navigation enhancer across jobseeker routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { AppRoot } from './AppRoot';
 import { ScoutRouteRoot } from './ScoutRouteRoot';
+import { ScoutNavigationEnhancer } from './components/ScoutNavigationEnhancer';
 import './styles.css';
 import './verified-workplace.css';
 
@@ -9,6 +10,9 @@ const root = window.location.pathname.startsWith('/scouts') ? <ScoutRouteRoot />
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {root}
+    <>
+      {root}
+      <ScoutNavigationEnhancer />
+    </>
   </StrictMode>,
 );
